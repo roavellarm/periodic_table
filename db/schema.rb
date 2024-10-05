0f3ce3fe7b4668acfcf4ffcb3abf469f87a2566c
Rodrigo Avellar <roavellarm@gmail.com>
2024-10-04 21:05:09 -0300

    Add create_orders migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_04_234810) do
+ActiveRecord::Schema.define(version: 2024_10_05_000338) do
 
   create_table "elements", force: :cascade do |t|
     t.string "name"
@@ -41,4 +41,12 @@ ActiveRecord::Schema.define(version: 2024_10_04_234810) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.integer "element_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["element_id"], name: "index_orders_on_element_id"
+  end
+
+  add_foreign_key "orders", "elements"
 end
