5db22354878677dd4060756ea683fff08a8d8941
Rodrigo Avellar <roavellarm@gmail.com>
2024-10-05 15:54:39 -0300

    Update TableController to use DB's data

--- app/controllers/table_controller.rb
+++ app/controllers/table_controller.rb
@@ -1,6 +1,19 @@
 class TableController < ApplicationController
   def index
-    file = File.read("db/data.json")
-    @elements = JSON.parse(file)
+    @elements = formated_elements_data
+  end
+
+  private
+
+  def elements
+    Element.joins(:order).includes(:shells, :ionization_energies).order('orders.id')
+  end
+
+  def formated_elements_data
+    elements.each_with_object({ "order" => [] }) do |element, hash|
+      element_key = element.name.parameterize(separator: '_')
+      hash["order"] << element_key
+      hash[element_key] = element.as_json
+    end
   end
 end
